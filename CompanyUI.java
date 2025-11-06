288f8f39a8fb79f5a509b5f548050116b501f54a
carin <161580454+gniyez@users.noreply.github.com>
2025-11-06 12:05:19 +0800

    .

--- CompanyUI.java
+++ CompanyUI.java
@@ -1,1 +1,25 @@
-public class CompanyUI implements 
+public class CompanyUI implements FilterOptions, LogIn, CompanyApplications{
+    public void createInternship(){
+
+    }
+
+    public void viewApplications(){
+
+    }
+
+    public void approveApplications(){
+
+    }
+
+    public void rejectApplications(){
+
+    }
+
+    public void toggleVisibility(String title, boolean visible){
+
+    }
+
+    public void filterApplications(String criteria, String value){
+        
+    }
+}
