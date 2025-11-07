bf39f874a5b52a28f0b22593b0839e338c09f16a
carin <161580454+gniyez@users.noreply.github.com>
2025-11-07 23:37:51 +0800

    Delete StudentApplications.java

--- StudentApplications.java
+++
@@ -1,5 +0,0 @@
-public interface StudentApplications{
-    void applyInternship(Student student, Internship internship);
-    void withdrawApplication(Student student, Application app);
-    void acceptPlacement(Student student, Application app);
-}
