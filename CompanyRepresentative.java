e3bdf02bb32fed872dd178dc6740522b4b2d994f
carin <161580454+gniyez@users.noreply.github.com>
2025-11-07 23:30:39 +0800

    Update CompanyRepresentative.java

--- CompanyRepresentative.java
+++ CompanyRepresentative.java
@@ -1,24 +1,26 @@
 public class CompanyRepresentative extends User{
-    private String companyID;
     private String companyName;
     private String department;
     private String position;
     private String status = "PENDING";
 
-    public CompanyRepresentative(String userID, String name, String companyID, String companyName, String department, String position) {
-        super(userID, name);
-        this.companyID = companyID;
+    public CompanyRepresentative(String email, String name, String companyName, 
+            String department, String position) {
+	super(email, name);
+	this.companyName = companyName;
+	this.department = department;
+	this.position = position;
+    this.status = "PENDING";
+    }
+    
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
         this.companyName = companyName;
-        this.department = department;
-        this.position = position;
-        this.status = "PENDING";
     }
-    public String getCompanyID(){
-        return companyID;
-    }
-    public void setCompanyID(String companyID){
-        this.companyID = companyID;
-    }
+    
     public String getDepartment(){
         return department;
     }
@@ -43,9 +45,7 @@ public class CompanyRepresentative extends User{
     public void setApproved(){
         this.status = "APPROVED";
     }
-    public String getCompanyName(){
-        return companyName;
-    }
+   
     public void displayRole(){
         System.out.println("I am a company representative");
     }
