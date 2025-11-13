e3e9d6b79021e44da2da3cb754aa7633e50cbf2e
carin <161580454+gniyez@users.noreply.github.com>
2025-11-13 13:12:00 +0800

    update

--- project/src/project/User.java
+++ project/src/project/User.java
@@ -1,11 +1,25 @@
 package project;
 
+/**
+ * Abstract class representing a user in the internship management system.
+ * Includes common attributes and methods for all user types.
+ * 
+ * @author Team BACF (Carin, Leyi, Pei Shan, Serene, Zhen Ying)
+ * @version 1.0
+ * @since 2025
+ */
+
 public abstract class User{
     private String userID;
     private String name;
     private String password= "password";
 
-   
+    /**
+     * Constructor to initialize User object with userID and name.
+     * 
+     * @param userID Unique identifier for user
+     * @param name Name of user
+     */
     public User(String userID,String name){
         this.userID = userID;
         this.name = name;
@@ -30,5 +44,10 @@ public abstract class User{
     public void setPassword(String password) {
         this.password = password;
     }
+
+    /**
+     * Abstract method to display the role of the user.
+     * Implemented by concrete subclasses.
+     */
     public abstract void displayRole();
 }
