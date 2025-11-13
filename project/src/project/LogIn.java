77189ee66e80ab23f3338003be65c732247ea73e
carin <161580454+gniyez@users.noreply.github.com>
2025-11-13 12:49:06 +0800

    update

--- project/src/project/LogIn.java
+++
@@ -1,7 +0,0 @@
-package project;
-
-public interface LogIn{
-    boolean login(String userID, String password);
-    void logout();
-    void changePassword(String userID, String oldPassword, String newPassword);
-}
