122e9fab49a9307b387dc87b095c3d60c8269274
zhenying <zhenyinglee7@gmail.com>
2025-11-16 16:04:36 +0800

    update

--- project/src/project/LogIn.java
+++ project/src/project/LogIn.java
@@ -1,7 +1,27 @@
 package project;
 
+/**
+ * Interface defining login-related operations for users.
+ */
 public interface LogIn{
+    /**
+     * Attempts to log in a user with the given credentials.
+     *
+     * @param userID   the user ID (usually email)
+     * @param password the user's password
+     * @return true if login succeeds, false otherwise
+     */
     boolean login(String userID, String password);
+    /**
+     * Logs out the current user.
+     */
     void logout();
+    /**
+     * Changes the password of a user.
+     *
+     * @param userID      the user ID
+     * @param oldPassword the current password
+     * @param newPassword the new password
+     */
     void changePassword(String userID, String oldPassword, String newPassword);
 }
